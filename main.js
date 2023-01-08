ee6e6cc5f3c44fb32d66e87d5337ea9447600149
adelguit <adeelguitoun@gmail.com>
2023-01-08 16:22:47 +0100

    Refactor menu toggle functionality

--- main.js
+++ main.js
@@ -1,36 +1,21 @@
+import NavigationMenu from './scripts/modules/nav.js';
+
 const menuBtn = document.querySelector('.menu-btn');
 const menuBtnIcons = menuBtn.querySelectorAll('.menu-btn img');
 const menu = document.querySelector('.nav-list');
 const menuLinks = menu.querySelectorAll('.nav-list a');
 const body = document.querySelector('body');
 
-function showMenu() {
-  body.classList.toggle('overflow');
-  menu.classList.toggle('toggled-menu');
+const navigationMenu = new NavigationMenu(menu, menuBtnIcons, menuLinks);
 
-  menuBtnIcons.forEach((icon) => {
-    icon.classList.toggle('hidden');
-  });
+menuBtn.addEventListener('click', () => {
+  body.classList.toggle('overflow-hidden');
+  navigationMenu.toggleMenu();
+});
 
-  menuLinks.forEach((link) => {
-    link.classList.toggle('nav-link');
-  });
-}
-
-function hideMenu() {
-  body.classList.remove('overflow');
-  menu.classList.remove('toggled-menu');
-
-  menuBtnIcons.forEach((icon) => {
-    icon.classList.toggle('hidden');
-  });
-
-  menuLinks.forEach((link) => {
-    link.classList.remove('nav-link');
-  });
-}
-
-menuBtn.addEventListener('click', showMenu);
 menuLinks.forEach((link) => {
-  link.addEventListener('click', hideMenu);
+  link.addEventListener('click', () => {
+    body.classList.remove('overflow-hidden');
+    navigationMenu.hideMenu();
+  });
 });
